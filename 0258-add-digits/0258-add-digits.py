04a64ec9b619636044eaae393832bd23414e01a2
Henok Tariku <150653375+Adam-7th@users.noreply.github.com>
2025-08-03 21:05:33 +0300

    Time: 1 ms (26.26%), Space: 17.7 MB (60.43%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,8 +1,6 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        num_l = list(map(int, str(num)))
-        while len(num_l) > 1:
-            total = sum(num_l)
-            num_l = list(map(int, str(total)))
-        return (num_l[0])
+        while num >= 10:
+            num = sum(map(int, str(num)))
+        return num
 Solution()
